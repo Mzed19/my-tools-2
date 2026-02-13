a6e2bb4a6672b41a119e69f64fc68b835a34f1b8
Michael Dumont <74337976+Mzed19@users.noreply.github.com>
2026-02-12 21:52:05 -0300

    Fix vite port

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
@@ -10,12 +7,13 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
     server: {
-        cors: true,
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
         },
     },
-});
+})
